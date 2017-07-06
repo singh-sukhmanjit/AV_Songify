271b51c835078b28fff9ab56c839684077ac1f2e
Sukhmanjit <singh.sukhmanjit@gmail.com>
2017-07-06 19:14:51 +0530

    Piano using functions, but with bug on click at label

--- main.js
+++ main.js
@@ -148,6 +148,9 @@ function toggleSong(){
         $('#songs').DataTable({
         paging:false       //pagination object disabled
         });
+
+        white_keys();
+        black_keys();
         
     }
 
@@ -284,7 +287,7 @@ $('.fa-step-backward').on('click', function(){
 
 //prev song using 'p' on keyboard
 $('body').on('keypress', function(event){
-    if(event.key == "p" && event.target.tagName!=="INPUT"){
+    if(event.key == "" && event.target.tagName!=="INPUT"){
         var song = document.querySelector('audio');
         if(currentSong==1){ //agr first song hai to last song chlega
             //agr currentSong=1 then songs[1] mtlb 2nd song, thats why currentSong=4-1=3 is songs[3]= lastsong
@@ -320,146 +323,296 @@ $('#edm').on('click', function(){
     instrument=3;
 })
 
-$('#C_3').on('click', function(){
-    Synth.play(instrument, 'C', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="q"){
+        Synth.play(instrument, 'C', 3, 2);
+        $('#C_3').addClass('pressed');
+        console.log('ff');
+        setTimeout(function(){
+            $('#C_3').removeClass('pressed');
+            console.log('kf');
+        },300);
+        }
 })
 
-$('#D_3').on('click', function(){
-    Synth.play(instrument, 'D', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="q"){
+        Synth.play(instrument, 'C', 3, 2);
+    }
 })
 
-$('#E_3').on('click', function(){
-    Synth.play(instrument, 'E', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="w"){
+        Synth.play(instrument, 'D', 3, 2);
+    }
 })
 
-$('#F_3').on('click', function(){
-    Synth.play(instrument, 'F', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="e"){
+        Synth.play(instrument, 'E', 3, 2);
+    }
 })
 
-$('#G_3').on('click', function(){
-    Synth.play(instrument, 'G', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="r"){
+        Synth.play(instrument, 'F', 3, 2);
+    }
 })
 
-$('#A_3').on('click', function(){
-    Synth.play(instrument, 'A', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="t"){
+        Synth.play(instrument, 'G', 3, 2);
+    }
 })
 
-$('#B_3').on('click', function(){
-    Synth.play(instrument, 'B', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="y"){
+        Synth.play(instrument, 'A', 3, 2);
+    }
 })
 
-$('#C_4').on('click', function(){
-    Synth.play(instrument, 'C', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="u"){
+        Synth.play(instrument, 'B', 3, 2);
+    }
 })
 
-$('#D_4').on('click', function(){
-    Synth.play(instrument, 'D', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="i"){
+        Synth.play(instrument, 'C', 4, 2);
+    }
 })
 
-$('#E_4').on('click', function(){
-    Synth.play(instrument, 'E', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="o"){
+        Synth.play(instrument, 'D', 4, 2);
+    }
 })
 
-$('#F_4').on('click', function(){
-    Synth.play(instrument, 'F', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="p"){
+        Synth.play(instrument, 'E', 4, 2);
+    }
 })
 
-$('#G_4').on('click', function(){
-    Synth.play(instrument, 'G', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="["){
+        Synth.play(instrument, 'F', 4, 2);
+    }
 })
 
-$('#A_4').on('click', function(){
-    Synth.play(instrument, 'A', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="]"){
+        Synth.play(instrument, 'G', 4, 2);
+    }
 })
 
-$('#B_4').on('click', function(){
-    Synth.play(instrument, 'B', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="z"){
+        Synth.play(instrument, 'A', 4, 2);
+    }
 })
 
-$('#C_5').on('click', function(){
-    Synth.play(instrument, 'C', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="x"){
+        Synth.play(instrument, 'B', 4, 2);
+    }
 })
 
-$('#D_5').on('click', function(){
-    Synth.play(instrument, 'D', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="c"){
+        Synth.play(instrument, 'C', 5, 2);
+    }
 })
 
-$('#E_5').on('click', function(){
-    Synth.play(instrument, 'E', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="v"){
+        Synth.play(instrument, 'D', 5, 2);
+    }
 })
 
-$('#F_5').on('click', function(){
-    Synth.play(instrument, 'F', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="b"){
+        Synth.play(instrument, 'E', 5, 2);
+    }
 })
 
-$('#G_5').on('click', function(){
-    Synth.play(instrument, 'G', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="n"){
+        Synth.play(instrument, 'F', 5, 2);
+    }
 })
 
-$('#A_5').on('click', function(){
-    Synth.play(instrument, 'A', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="m"){
+        Synth.play(instrument, 'G', 5, 2);
+    }
 })
 
-$('#B_5').on('click', function(){
-    Synth.play(instrument, 'B', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key==","){
+        Synth.play(instrument, 'A', 5, 2);
+    }
 })
 
-$('#CH_3').on('click', function(){
-    Synth.play(instrument, 'C#', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="."){
+        Synth.play(instrument, 'B', 5, 2);
+    }
 })
 
-$('#DH_3').on('click', function(){
-    Synth.play(instrument, 'D#', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="2"){
+        Synth.play(instrument, 'C#', 3, 2);
+    }
 })
 
-$('#FH_3').on('click', function(){
-    Synth.play(instrument, 'F#', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="3"){
+        Synth.play(instrument, 'D#', 3, 2);
+    }
 })
 
-$('#GH_3').on('click', function(){
-    Synth.play(instrument, 'G#', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="5"){
+        Synth.play(instrument, 'F#', 3, 2);
+    }
 })
 
-$('#AH_3').on('click', function(){
-    Synth.play(instrument, 'A#', 3, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="6"){
+        Synth.play(instrument, 'G#', 3, 2);
+    }
 })
 
-$('#CH_4').on('click', function(){
-    Synth.play(instrument, 'C#', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="7"){
+        Synth.play(instrument, 'A#', 3, 2);
+    }
 })
 
-$('#DH_4').on('click', function(){
-    Synth.play(instrument, 'D#', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="9"){
+        Synth.play(instrument, 'C#', 4, 2);
+    }
 })
 
-$('#FH_4').on('click', function(){
-    Synth.play(instrument, 'F#', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="0"){
+        Synth.play(instrument, 'D#', 4, 2);
+    }
 })
 
-$('#GH_4').on('click', function(){
-    Synth.play(instrument, 'G#', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="="){
+        Synth.play(instrument, 'F#', 4, 2);
+    }
 })
 
-$('#AH_4').on('click', function(){
-    Synth.play(instrument, 'A#', 4, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="a"){
+        Synth.play(instrument, 'G#', 4, 2);
+    }
 })
 
-$('#CH_5').on('click', function(){
-    Synth.play(instrument, 'C#', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="s"){
+        Synth.play(instrument, 'A#', 4, 2);
+    }
 })
 
-$('#DH_5').on('click', function(){
-    Synth.play(instrument, 'D#', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="f"){
+        Synth.play(instrument, 'C#', 4, 2);
+    }
 })
 
-$('#FH_5').on('click', function(){
-    Synth.play(instrument, 'F#', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="g"){
+        Synth.play(instrument, 'D#', 4, 2);
+    }
 })
 
-$('#GH_5').on('click', function(){
-    Synth.play(instrument, 'G#', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="j"){
+        Synth.play(instrument, 'F#', 4, 2);
+    }
 })
 
-$('#AH_5').on('click', function(){
-    Synth.play(instrument, 'A#', 5, 2);
+$('body').on('keypress', function(event){
+    if(event.key=="k"){
+        Synth.play(instrument, 'G#', 4, 2);
+    }
+})
+
+$('body').on('keypress', function(event){
+    if(event.key=="l"){
+        Synth.play(instrument, 'A#', 4, 2);
+    }
+})
+
+function white_keys(){
+    var keys=['C3','D3','E3','F3','G3','A3','B3',
+              'C4','D4','E4','F4','G4','A4','B4',
+              'C5','D5','E5','F5','G5','A5','B5'];
+    var press=['Q','W','E','R','T','Y','U',
+               'I','O','P','[',']','Z','X',
+               'C','V','B','N','M',',','.'
+               ];
+    for(var i=0; i<keys.length; i++){
+        console.log(keys[i]);
+        var id='#w'+(i+1);
+        $(id).find('.keyHead').text(press[i]);
+        $(id).find('.keyFoot').text(keys[i]);
+    }    
+}
+
+function black_keys(){
+    var keys=['C3#','D3#','F3#','G3#','A3#',
+              'C4#','D4#','F4#','G4#','A4#',
+              'C5#','D5#','F5#','G5#','A5#'];
+    var press=['2','3','5','6','7',
+               '9','0','=','A','S',
+               'F','G','J','K','L'];
+    for(var i=0; i<keys.length; i++){
+        console.log(keys[i]);
+        var id='#b'+(i+1);
+        $(id).find('.keyHead').text(press[i]);
+        $(id).find('.keyFoot').text(keys[i]);
+    }    
+}
+
+var music=[
+        {'note': 'C', 'octave':'3'},{'note': 'D', 'octave':'3'},{'note': 'E', 'octave':'3'},
+        {'note': 'F', 'octave':'3'},{'note': 'G', 'octave':'3'},{'note': 'A', 'octave':'3'},
+        {'note': 'B', 'octave':'3'},{'note': 'C', 'octave':'4'},{'note': 'D', 'octave':'4'},
+        {'note': 'E', 'octave':'4'},{'note': 'F', 'octave':'4'},{'note': 'G', 'octave':'4'},
+        {'note': 'A', 'octave':'4'},{'note': 'B', 'octave':'4'},{'note': 'C', 'octave':'5'},
+        {'note': 'D', 'octave':'5'},{'note': 'E', 'octave':'5'},{'note': 'F', 'octave':'5'},
+        {'note': 'G', 'octave':'5'},{'note': 'A', 'octave':'5'},{'note': 'B', 'octave':'5'},
+        {'note': 'C#', 'octave':'3'},{'note': 'D#', 'octave':'3'},{'note': 'F#', 'octave':'3'},
+        {'note': 'G#', 'octave':'3'},{'note': 'A#', 'octave':'3'},{'note': 'C#', 'octave':'4'},
+        {'note': 'D#', 'octave':'4'},{'note': 'F#', 'octave':'4'},{'note': 'G#', 'octave':'4'},
+        {'note': 'A#', 'octave':'4'},{'note': 'C#', 'octave':'5'},{'note': 'D#', 'octave':'5'},
+        {'note': 'F#', 'octave':'5'},{'note': 'G#', 'octave':'5'},{'note': 'A#', 'octave':'5'}        
+]
+
+var music_id=['w1','w2','w3','w4','w5','w6','w7','w8','w9','w10','w11','w12',
+              'w13','w14','w15','w16','w17','w18','w19','w20','w21','b1','b2','b3',
+              'b4','b5','b6','b7','b8','b9','b10','b11','b12','b13','b14','b15']
+
+$('body').on('click', function(event){
+    console.log(event);
+    var id =event.target.id;
+    console.log(id);
+    for(var i=0; i<music.length; i++){
+        if(music_id[i]==id){
+            console.log('me chal raha hu');
+            break;
+        }
+        
+    }
+    console.log(music.length);
+    Synth.play(instrument, music[i].note, music[i].octave, 2);
+    
 })
